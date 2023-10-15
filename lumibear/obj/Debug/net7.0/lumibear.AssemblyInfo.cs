6a800f729f48175d7359df7db7b61190112b5944
Mateusz Augustyniak <s18531@pjwstk.edu.pl>
2023-10-15 21:26:05 +0200

    sliderek

--- lumibear/obj/Debug/net7.0/lumibear.AssemblyInfo.cs
+++ lumibear/obj/Debug/net7.0/lumibear.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lumibear")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f682e15e3045fd909c72a4b0973810a5943178d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5890136bceaf274d02f67cf42d41fbd6f4dfb868")]
 [assembly: System.Reflection.AssemblyProductAttribute("lumibear")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lumibear")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
